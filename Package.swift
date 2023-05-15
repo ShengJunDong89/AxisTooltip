c9274101de67565d352a5045ac8dbcb83dc046a8
Sheng Jun Dong <shengjundong89@gmail.com>
2023-05-15 19:55:12 -0400

    define path to sources

--- Package.swift
+++ Package.swift
@@ -24,9 +24,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AxisTooltip",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"
+        ),
+
         .testTarget(
             name: "AxisTooltipTests",
-            dependencies: ["AxisTooltip"]),
+            dependencies: ["AxisTooltip"]
+        )
     ]
 )
